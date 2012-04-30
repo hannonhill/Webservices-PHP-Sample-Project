a430543a965a2eefc9bed457b5619205a302983f
Artur Tomusiak <artur.tomusiak@hannonhill.com>
2012-04-30 10:19:54 -0400

    Re-verified that the sample project works with 7.0

--- test.php
+++ test.php
@@ -3,19 +3,21 @@ include("web_services_util.php");
 
 $client = new SoapClient ( "http://localhost:8080/ws/services/AssetOperationService?wsdl", array ('trace' => 1 ) );	
 $auth = array ('username' => 'admin', 'password' => 'admin' );
-$id = array ('type' => 'page', 'id' => '9e14f5d50a00019501d593b987275f82' );	
+$id = array ('type' => 'page', 'id' => 'b54d846f0a00016c6a59d8da787f87cf' );	
 $params = array ('authentication' => $auth, 'identifier' => $id );
 	
 // Read asset
 $reply = $client->read ( $params );
-if ($reply->readReturn->success == 'true') {
+if ($reply->readReturn->success == 'true') 
+{
 	$asset = ( array ) $reply->readReturn->asset->page;
-	print_r($asset);
+
 	// Get its title
 	$metadata = $asset["metadata"];
 	$title = $metadata->title;
 	
 	// Update title
+	date_default_timezone_set('America/New_York');
 	$title = date('l dS \of F Y h:i:s A');
 	$metadata->title = $title;
 	
